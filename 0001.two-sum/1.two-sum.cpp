ec39fdd392efd427ceef090dfd97fa1df0f1f843
Alexey Smychagin <smalex69@gmail.com>
2021-11-26 10:00:32 -0500

    change layout

---
+++ 0001.two-sum/1.two-sum.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (47.86%)
+ * Total Accepted:    5.3M
+ * Total Submissions: 11.1M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * You can return the answer in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * Only one valid answer exists.
+ * 
+ * 
+ * 
+ * Follow-up: Can you come up with an algorithm that is less than O(n^2) time
+ * complexity?
+ */
+class Solution {
+  public:
+    vector<int> twoSum(const vector<int>& nums, int target) {
+      map<int, int> mp;
+      for (int i = 0; i < nums.size(); i++) {
+        int x = target-nums[i];
+        if (mp.find(x) != mp.end()) {
+          return {mp[x], i};
+        }
+        mp[nums[i]] = i;
+      }
+      return {};
+    }
+};
+
+int main() {
+  Solution *sol = new Solution();
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> a[i];
+    }
+    vector<int> res = sol->twoSum(a, x);
+    for (int i : res) {
+      cout << i << " ";
+    }
+    cout << endl;
+  }
+}
