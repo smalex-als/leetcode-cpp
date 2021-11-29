3b50008f35d6215b212ded76a7f9e42118b85ab3
smalex-als <smalex69@gmail.com>
2021-11-29 09:44:55 -0500

    moved

--- 0239.sliding-window-maximum/239.sliding-window-maximum.cpp
+++ 0239.sliding-window-maximum/239.sliding-window-maximum.cpp
@@ -87,11 +87,22 @@ public:
 };
 
 int main() {
-  vector<int> nums = {1,3,-1,-3,5,3,6,7};
-// Output: [3,3,5,5,6,7]
-  auto sol = new Solution();
-  auto res = sol->maxSlidingWindow(nums, 3);
-  for (int i : res) {
-    cout << i << " ";
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> a[i];
+    }
+    Solution *sol = new Solution();
+    auto res = sol->maxSlidingWindow(a, x);
+    for (int i : res) {
+      cout << i << " ";
+    }
+    cout << endl;
   }
 }
