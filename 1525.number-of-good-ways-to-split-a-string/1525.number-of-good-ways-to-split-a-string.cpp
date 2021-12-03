55bf8910f432b074fb7bd074220c89f502b100a2
Alexey Smychagin <smalex69@gmail.com>
2021-12-03 09:49:59 -0500

    moved

--- 1525.number-of-good-ways-to-split-a-string/1525.number-of-good-ways-to-split-a-string.cpp
+++ 1525.number-of-good-ways-to-split-a-string/1525.number-of-good-ways-to-split-a-string.cpp
@@ -1,3 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+#include <array>
+#include <unordered_map>
+#include <queue>
+#include <unordered_set>
+#include <iomanip>
+#include <zconf.h>
+
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+
+using namespace std;
+
+typedef long long int64;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
 /*
  * @lc app=leetcode id=1525 lang=cpp
  *
@@ -90,3 +113,15 @@ public:
     return res;
   }
 };
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    Solution *sol = new Solution();
+    cout << sol->numSplits(s) << endl;
+  }
+}
