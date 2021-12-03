2a0ed16b7c7c1a0c49ff03490ea42717d47e1eb1
Alexey Smychagin <smalex69@gmail.com>
2021-12-03 09:55:50 -0500

    moved

--- 0283.move-zeroes/283.move-zeroes.cpp
+++ 0283.move-zeroes/283.move-zeroes.cpp
@@ -1,5 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+#include <array>
+#include <unordered_map>
+#include <queue>
+#include <unordered_set>
+#include <iomanip>
+#include <zconf.h>
+
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+
 using namespace std;
+
+typedef long long int64;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
 //Given an integer array nums, move all 0's to the end of it while maintaining 
 //the relative order of the non-zero elements. 
 //
@@ -26,7 +47,6 @@ using namespace std;
 //Topics Array Two Pointers 👍 6935 👎 193
 
 
-//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
   void moveZeroes(vector<int>& nums) {
@@ -41,11 +61,22 @@ public:
 };
 
 int main() {
-  vector<int> nums = {0,1,0,3,12};
-  auto sol = new Solution();
-  sol->moveZeroes(nums);
-  for (int i : nums) {
-    cout << i << " ";
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> nums[i];
+    }
+    auto sol = new Solution();
+    sol->moveZeroes(nums);
+    for (int i : nums) {
+      cout << i << " ";
+    }
+    cout << endl;
   }
-  cout << endl;
 }
