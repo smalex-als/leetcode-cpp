a5ce31c1787859e79b53bb03d82899fe6e9641cf
Alexey Smychagin <smalex69@gmail.com>
2021-12-07 22:10:44 -0500

    accepted

---
+++ 0045.jump-game-ii/45.jump-game-ii.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+#include <array>
+#include <unordered_map>
+#include <queue>
+#include <unordered_set>
+#include <iomanip>
+#include <zconf.h>
+
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+
+using namespace std;
+
+typedef long long int64;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+/*
+ * @lc app=leetcode id=45 lang=cpp
+ *
+ * [45] Jump Game II
+ *
+ * https://leetcode.com/problems/jump-game-ii/description/
+ *
+ * algorithms
+ * Medium (35.28%)
+ * Total Accepted:    490.5K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * Given an array of non-negative integers nums, you are initially positioned
+ * at the first index of the array.
+ * 
+ * Each element in the array represents your maximum jump length at that
+ * position.
+ * 
+ * Your goal is to reach the last index in the minimum number of jumps.
+ * 
+ * You can assume that you can always reach the last index.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [2,3,1,1,4]
+ * Output: 2
+ * Explanation: The minimum number of jumps to reach the last index is 2. Jump
+ * 1 step from index 0 to 1, then 3 steps to the last index.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [2,3,0,1,4]
+ * Output: 2
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * 0 <= nums[i] <= 1000
+ * 
+ * 
+ */
+class Solution {
+public:
+  int jumpPrev(vector<int>& nums) {
+    return 0;
+  }
+  int jump(vector<int>& nums) {
+    if (nums.size() < 2) {
+      return 0;
+    }
+    int ans = 0;
+    int mx = 0;
+    int next_mx = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (i > mx) {
+        mx = next_mx;
+        ans++;
+      }
+      next_mx = max(next_mx, i + nums[i]);
+    }
+    return ans;
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+    }
+    Solution *sol = new Solution();
+    cout << sol->jump(a) << endl;
+  }
+}
