8d7ccc564ce7490ae704194b77dd277d654d462c
smalex-als <smalex69@gmail.com>
2021-12-09 13:00:10 -0500

    accepted

---
+++ 0098.validate-binary-search-tree/98.validate-binary-search-tree.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+#include <array>
+#include <unordered_map>
+#include <queue>
+#include <unordered_set>
+#include <iomanip>
+#include <zconf.h>
+#include "tree.hpp"
+
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+
+using namespace std;
+
+typedef long long int64;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (29.79%)
+ * Total Accepted:    1.2M
+ * Total Submissions: 4M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Given the root of a binary tree, determine if it is a valid binary search
+ * tree (BST).
+ * 
+ * A valid BST is defined as follows:
+ * 
+ * 
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [2,1,3]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is
+ * 4.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [1, 10^4].
+ * -2^31 <= Node.val <= 2^31 - 1
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  bool traverse(TreeNode* node, TreeNode* l, TreeNode *r) {
+    if (node == nullptr) {
+      return true;
+    }
+    if (l && l->val >= node->val) {
+      return false;
+    }
+    if (r && r->val <= node->val) {
+      return false;
+    }
+    return traverse(node->left, l, node) && traverse(node->right, node, r);
+  }
+
+  bool isValidBST(TreeNode* root) {
+    return traverse(root, nullptr, nullptr);
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      string s;
+      cin >> s;
+      int val = INT_MAX;
+      if (s != "null") {
+        val = stoi(s);
+      }
+      a[i] = val;
+    }
+    TreeNode *tree = makeTree(a);
+    Solution *sol = new Solution();
+    cout << sol->isValidBST(tree) << endl;
+  }
+}
