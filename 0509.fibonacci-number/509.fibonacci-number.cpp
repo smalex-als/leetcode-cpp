0efe7b2fca11caccc01bf625cde71fbe1384039b
Alexey Smychagin <smalex69@gmail.com>
2022-01-17 14:11:08 -0500

    add a problem

--- 0509.fibonacci-number/509.fibonacci-number.cpp
+++ 0509.fibonacci-number/509.fibonacci-number.cpp
@@ -40,38 +40,6 @@ typedef pair<int, int> ii;
  * 
  * Given n, calculate F(n).
  * 
- * 
- * Example 1:
- * 
- * 
- * Input: n = 2
- * Output: 1
- * Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
- * 
- * 
- * Example 2:
- * 
- * 
- * Input: n = 3
- * Output: 2
- * Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
- * 
- * 
- * Example 3:
- * 
- * 
- * Input: n = 4
- * Output: 3
- * Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
- * 
- * 
- * 
- * Constraints:
- * 
- * 
- * 0 <= n <= 30
- * 
- * 
  */
 class Solution {
 public:
